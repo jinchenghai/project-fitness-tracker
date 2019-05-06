4b08fe8b413f0563ff33320a9748899f5faf8905
agrusz1 <a.gruszczynski@outlook.com>
2019-05-06 00:22:31 -0500

    ADD: src/test/java, FTATest | Added test folder with dummy test.

---
+++ src/test/java/edu/depaul/fitnesstracker/FitnessTrackerAppTest.java
@@ -0,0 +1,25 @@
+package edu.depaul.fitnesstracker;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FitnessTrackerAppTest {
+
+	 /**
+     * Looking into more information about testing Main Class (testing app is running)
+     */
+	@Test
+    public void testApp()
+    {
+        assertTrue(true);
+    }
+
+}
