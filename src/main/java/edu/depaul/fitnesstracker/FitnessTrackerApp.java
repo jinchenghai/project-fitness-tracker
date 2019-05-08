ffbf3971f0ef3ca5bdded8c723f65bb54be60b61
agrusz1 <a.gruszczynski@outlook.com>
2019-05-08 06:31:23 -0500

    ADD: FitnessTrackerApp.java | Added configure for JSP use.

--- src/main/java/edu/depaul/fitnesstracker/FitnessTrackerApp.java
+++ src/main/java/edu/depaul/fitnesstracker/FitnessTrackerApp.java
@@ -2,10 +2,16 @@ package edu.depaul.fitnesstracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FitnessTrackerApp {
+public class FitnessTrackerApp extends SpringBootServletInitializer  {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(FitnessTrackerApp.class);
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessTrackerApp.class, args);
 	}
